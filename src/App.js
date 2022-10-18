d86f6a06012091a9c9a089a4ff65025ba40b26ce
kairat <kairatkubanychbekov96@gmail.com>
2022-10-18 23:32:57 +0300

    homeWork-7.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
 
 function App() {
+  const [firstInput, setFirstInput] = useState(" ");
+  const [twoInput, setTwoInput] = useState("");
+  const [theeInput, setThreeInput] = useState("");
+
+  const changeInputValue = (event) => {
+    setFirstInput(event.target.value);
+  };
+  const twoInputValue = (event) => {
+    setTwoInput(event.target.value);
+  };
+  const threeInputValue = (event) => {
+    setThreeInput(event.target.value);
+  };
+
+  const addData = () => {
+    const data = {
+      name: firstInput,
+      age: twoInput,
+      num: theeInput,
+    };
+    console.log(data);
+
+    setFirstInput("");
+    setTwoInput("");
+    setThreeInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="first-input">
+        <label htmlFor="add"> name </label>
+        <input
+          className="input"
+          type="text"
+          id="add"
+          value={firstInput}
+          onChange={changeInputValue}
+        />
+      </div>
+      <div>
+        <label htmlFor="add"> age </label>
+        <input
+          className="input"
+          type="text"
+          id="add"
+          value={twoInput}
+          onChange={twoInputValue}
+        />
+      </div>
+      <div className="three-input">
+        <label htmlFor="add"> num </label>
+        <input
+          className="input"
+          type="text"
+          id="add"
+          value={theeInput}
+          onChange={threeInputValue}
+        />
+      </div>
+      <div>
+        <button onClick={addData}> add </button>
+      </div>
+    
     </div>
   );
 }
